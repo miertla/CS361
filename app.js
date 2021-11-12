59cc3ddc0d72bdcd323546550b60ed66d2cece1a
miertla <miertla@oregonstate.edu>
2021-11-12 01:03:35 -0500

    fixed error

--- app.js
+++ app.js
@@ -11,8 +11,18 @@ const pool = new Pool({
   }
 });
 
-/*
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 client.connect();
+
 
 client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
   if (err) throw err;
@@ -21,7 +31,7 @@ client.query('SELECT table_schema,table_name FROM information_schema.tables;', (
   }
   client.end();
 });
-*/
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
