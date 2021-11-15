ec23408b6d37e32efe6c6589c55376429cd52fa2
miertla <miertla@oregonstate.edu>
2021-11-15 02:48:06 -0500

    working on service

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-const { Pool } = require('pg');
+
+
+const { Pool } = require('pg')
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:<jyqNe9-josfaw-vyhkup>@localhost:5000/<postgresql-trapezoidal-77382>',
+  ssl: process.env.DATABASE_URL ? true : false
 });
+
+const { Client } = require('pg')
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+ 
+});
+
+client.connect();
+
+
+
 
 /*
 client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
@@ -29,28 +40,40 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/home'))
   .get('/userHomepage', (req, res) => res.render('pages/userHomepage'))
-  .get('/wishList', (req, res) => res.render('pages/wishList'))
+//  .get('/wishList', (req, res) => res.send(getData()))
   .get('/addToJournal', (req, res) => res.render('pages/addToJournal'))
   .get('/addToWishList', (req, res) => res.render('pages/addToWishList'))
   .get('/createProfile', (req, res) => res.render('pages/createProfile'))
   .get('/editTrip', (req, res) => res.render('pages/editTrip'))
   .get('/search', (req, res) => res.render('pages/search'))
-   
-/*
-.get('/wishList', async (req, res) => {
+  .get('/wishList', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM wish_list');
+      const result = await client.query('SELECT location, text_description FROM wish_list');
       const results = { 'results': (result) ? result.rows : null};
-        res.render('pages/wishList', { wishListLocations : results });
+      res.render('pages/wishList', results );
       client.release();
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
     }
   })
-*/
-
-
-
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
+
+
+/*
+getData = () => {
+// function to display all locations in wish_list table
+  pool.query('SELECT location, text_description FROM wish_list;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  pool.end();
+  res.send('pages/wishList', { wish_list: res.rows });
+});
+}
+*/
--- public/javascripts/addToWishList.js
+++ public/javascripts/addToWishList.js
@@ -5,12 +5,11 @@ module.exports = function(){
 
 /* Adds a location to the users wishlist & redirects to wishList page after adding */
     router.post('/', function(req, res){
-        consolelog(req.body.)
         console.log(req.body)
-	/*** EDIT ***/
-        var sql = "INSERT INTO wish_list (location, textDescription) VALUES (?,?)";
+	/*** EDIT/FIX ***/
+        var query-string = `INSERT INTO wish_list(location, text_descrition) VALUES (?,?)`;
         var inserts = [req.body.location, req.body.paragraph1];
-        sql = pool.query(sql,inserts,function(error, results, fields){
+        pool.query(query-string, (error, results, fields) => {
             if(error){
                 console.log(JSON.stringify(error))
                 res.write(JSON.stringify(error));
