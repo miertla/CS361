c46bcca202f530dcee514ef1215df1596a5d4294
miertla <miertla@oregonstate.edu>
2021-11-15 22:45:36 -0500

    working on queries

--- app.js
+++ app.js
@@ -3,23 +3,15 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
 
-
-const { Pool } = require('pg')
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:<jyqNe9-josfaw-vyhkup>@localhost:5000/<postgresql-trapezoidal-77382>',
-  ssl: process.env.DATABASE_URL ? true : false
+var prodClient = new Pool({
+  // production connection string:
+  connectionString: process.env.DATABASE_URL
 });
+db = prodClient;
 
-const { Client } = require('pg')
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
- 
-});
-
-client.connect();
-
-
+db.connect();
 
 
 /*
@@ -40,40 +32,23 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/home'))
   .get('/userHomepage', (req, res) => res.render('pages/userHomepage'))
-//  .get('/wishList', (req, res) => res.send(getData()))
+  .get('/wishList', (req, res) => res.render('pages/wishList'))
   .get('/addToJournal', (req, res) => res.render('pages/addToJournal'))
   .get('/addToWishList', (req, res) => res.render('pages/addToWishList'))
   .get('/createProfile', (req, res) => res.render('pages/createProfile'))
   .get('/editTrip', (req, res) => res.render('pages/editTrip'))
   .get('/search', (req, res) => res.render('pages/search'))
+ /*
   .get('/wishList', async (req, res) => {
     try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT location, text_description FROM wish_list');
+      const result = await db.query('SELECT location, text_description FROM wish_list;');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/wishList', results );
-      client.release();
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
     }
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-
-
-
-/*
-getData = () => {
-// function to display all locations in wish_list table
-  pool.query('SELECT location, text_description FROM wish_list;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  pool.end();
-  res.send('pages/wishList', { wish_list: res.rows });
-});
-}
 */
+
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
